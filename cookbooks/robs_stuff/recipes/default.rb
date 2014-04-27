83bd6043b71094045734bda7c7ef54f8650e0e2c
Rob Davis <rtdavis22@gmail.com>
2014-04-27 01:12:21 -0700

    Add public/private keys to home directory.

--- cookbooks/robs_stuff/recipes/default.rb
+++ cookbooks/robs_stuff/recipes/default.rb
@@ -12,7 +12,23 @@ user "robert" do
   shell "/bin/bash"
 end
 
+directory "/home/robert/.ssh" do
+  owner "robert"
+  group "robert"
+end
+
 rob_key = Chef::EncryptedDataBagItem.load("ssh_keys", "rob")
-file "/home/robert/id_rsa" do
+file "/home/robert/.ssh/id_rsa" do
+  owner "robert"
+  group "robert"
+  mode "600"
   content rob_key["private_key"]
+end
+
+public_key = data_bag_item("ssh_keys", "rob")["public_key"]
+file "/home/robert/.ssh/authorized_keys" do
+  owner "robert"
+  group "robert"
+  mode "644"
+  content public_key
 end
